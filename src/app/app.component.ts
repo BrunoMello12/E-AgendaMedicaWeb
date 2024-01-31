a81b13882b94e8ef80da98346805ef6fa1f6edc6
Bruno <Cliente@DESKTOP-2TBK80U>
2024-01-31 15:16:14 -0300

    implementado o loading

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { LoadingService } from './core/loading/services/loading.service';
+import {
+  Event,
+  NavigationCancel,
+  NavigationEnd,
+  NavigationError,
+  NavigationStart,
+  Router,
+} from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  estaCarregando$?: Observable<boolean>;
 
+  constructor(private LoadingService: LoadingService, private router: Router){
+    this.router.events.subscribe((event: Event) =>
+      this.atualizarStatusCarregamento(event)
+    );
+  }
+
+  ngOnInit(): void {
+    this.estaCarregando$ = this.LoadingService.obterStatusCarregamento();
+  }
+
+  atualizarStatusCarregamento(event: Event): void {
+    if (event instanceof NavigationStart) {
+      this.LoadingService.carregar();
+    } else if (
+      event instanceof NavigationEnd ||
+      event instanceof NavigationCancel ||
+      event instanceof NavigationError
+    ) {
+      this.LoadingService.parar();
+    }
+  }
 }
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,6 +3,7 @@ import { ShellModule } from './shell/shell.module';
 import { NotificationModule } from './notification/notification.module';
 import { CommonModule } from '@angular/common';
 import { AppRoutingModule } from '../app-routing.module';
+import { LoadingModule } from './loading/loading.module';
 
 @NgModule({
   declarations: [],
@@ -10,8 +11,9 @@ import { AppRoutingModule } from '../app-routing.module';
     CommonModule,
     ShellModule,
     AppRoutingModule,
-    NotificationModule
+    NotificationModule,
+    LoadingModule
   ],
-  exports: [ShellModule],
+  exports: [ShellModule, LoadingModule],
 })
 export class CoreModule { }
